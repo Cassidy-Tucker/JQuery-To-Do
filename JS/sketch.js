c1ac1769ffed586ca15536fcf014145d22bd91db
Keesha Cain <keeshacain@Keeshas-MacBook-Pro.local>
2017-10-02 15:19:38 -0600

    Keesha updated files

---
+++ JS/sketch.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){});
+
+function setup(){
+  createCanvas(window.innerWidth, window.innerHeight);
+
+  background('white');
+
+  fill('tan');
+
+  strokeWeight(.5)
+
+
+  scale(10.0,10.0)
+  quad(20,30,40,30,35,50,25,50)
+  quad(70,30,90,30,85,50,75,50)
+  text("Tasks",20,60)
+
+}
+
+function draw(){};
