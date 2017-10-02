0d36045c71d1de18f77264670aa57a749f82af19
Cassidy <macair@Cassidys-MacBook-Air.local>
2017-10-02 17:31:52 -0600

    open new branch for p5 animation

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,20 +1,39 @@
 $(document).ready(function(){});
 
-function setup(){
-  createCanvas(window.innerWidth, window.innerHeight);
+basket1= [width/4, height/2];
+basket2= [(width/4) * 3, height/2];
 
-  background('white');
+//var xStart = 30;
+//var xEnd = 80;
 
-  fill('tan');
-
-  strokeWeight(.5)
-
-
+function drawBaskets () {
   scale(10.0,10.0)
   quad(20,30,40,30,35,50,25,50)
   quad(70,30,90,30,85,50,75,50)
   text("Tasks",20,60)
+  text("Done",70,60)
 
+  ellipse(30, 30, 5, 5);
 }
 
-function draw(){};
+function setup(){
+  createCanvas(window.innerWidth, window.innerHeight);
+  background('white');
+  fill('tan');
+  strokeWeight(.5)
+  drawBaskets();
+}
+
+function draw() {
+  background(color(255));
+  drawBaskets();
+
+  //xStart = xStart +2;
+
+  //if(xStart < xEnd){
+  //  ellipse(xStart, 30, 5, 5);
+  //}
+
+  lerp(basket1[0], basket2[0], .2);
+
+}
